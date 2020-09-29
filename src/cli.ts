63f355654150ff92806b583d2a875b085b83142e
LFDM <1986gh@gmail.com>
2020-09-29 13:01:49 +0200

    Remove old development code

--- src/cli.ts
+++ src/cli.ts
@@ -1,26 +1,7 @@
 import * as commander from "commander";
-import * as path from "path";
 import { generate, Options } from "./generate";
 import { readFile } from "./util";
 import { watch } from "./watch";
-
-const ROOT = path.join(__dirname, "..", "..", "..", "..");
-const FOLDER = path.join(ROOT, "shared", "locales", "en");
-const OUT = path.join(ROOT, "app", "src", "i18next_types.ts");
-
-console.log(process.cwd(), __dirname);
-
-// generate({
-//   inFolder: FOLDER,
-//   outFile: OUT
-// });
-
-watch({ inFolder: FOLDER }, () =>
-  generate({
-    inFolder: FOLDER,
-    outFile: OUT,
-  })
-);
 
 const withOptions = (command: commander.Command) => {
   return command
@@ -70,7 +51,6 @@ const parseOptions = async (opts: any): Promise<Options> => {
 withOptions(commander.command("generate").alias("g")).action(async (c) => {
   const options = await parseOptions(c.opts());
   await generate(options);
-  process.exit(0);
 });
 
 withOptions(commander.command("watch").alias("w")).action(async (c) => {
