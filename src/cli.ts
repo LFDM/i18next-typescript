eaf3509a42b02e29fbc2d0ae7090bbf9642bdaf8
LFDM <1986gh@gmail.com>
2020-09-29 12:35:01 +0200

    Define cli options

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,4 @@
+import * as commander from "commander";
 import * as path from "path";
 import { generate } from "./generate";
 import { watch } from "./watch";
@@ -19,3 +20,33 @@ watch({ inFolder: FOLDER }, () =>
     outFile: OUT,
   })
 );
+
+const withOptions = (command: commander.Command) => {
+  return command
+    .option(
+      "-i, --in <inFolder>",
+      "Path to a folder with your translation files"
+    )
+    .option("-o, --out <outFile>", "Path to the output file")
+    .option(
+      "--default-namespace <defaultNamespace>",
+      "Default Namespace in your i18next configuration",
+      "translation"
+    )
+    .option("--indent <indent>", "Indentation level of output file", "2")
+    .option(
+      "-t, --type-name, <typeName>",
+      "Exported type name in the generated file"
+    )
+    .option("--quote-char", "Character to use to quote strings", `'`)
+    .option(
+      "-c, --config",
+      "Path to config file",
+      "i18next-typescript.config.json"
+    );
+};
+
+withOptions(commander.command("generate").alias("g"));
+withOptions(commander.command("watch").alias("w"));
+
+commander.parse(process.argv);
